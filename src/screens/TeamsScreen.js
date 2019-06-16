b815d82ed37806c71d31b7489a4829acaf9ab9e1
Ryan Walker <rytwalker@gmail.com>
2019-06-16 17:55:33 -0400

    only show all teams on request

--- src/screens/TeamsScreen.js
+++ src/screens/TeamsScreen.js
@@ -1,36 +1,56 @@
 import React, { Component } from 'react';
-import { FlatList, ActivityIndicator } from 'react-native';
+import { FlatList, ActivityIndicator, Text, View } from 'react-native';
+import { Button, TabHeading } from 'native-base';
 import { connect } from 'react-redux';
 import { fetchTeams } from '../store/actions/index';
 import Team from '../components/Team/Team';
 
 class TeamsScreen extends Component {
-  componentDidMount() {
-    const { fetchTeams, teams } = this.props;
-    if (!teams.length) fetchTeams();
-  }
+  state = {
+    showTeams: false
+  };
+
+  // componentDidMount() {
+  //   const { teams } = this.props;
+  //   if (!teams.length) fetchTeams();
+  // }
+
+  handleFetchTeams = () => {
+    const { fetchTeams } = this.props;
+    this.setState({ showTeams: true }, () => fetchTeams());
+  };
+
   render() {
     const { teams } = this.props;
     return (
       <React.Fragment>
-        {!teams.length ? (
-          <ActivityIndicator />
-        ) : (
-          <FlatList
-            data={teams}
-            renderItem={({ item, index }) => {
-              return (
-                <Team
-                  team={{ ...item, rank: index + 1 }}
-                  index={index}
-                  handleTeamPress={() =>
-                    this.props.navigation.navigate('TeamCard', { item })
-                  }
-                />
-              );
-            }}
-            keyExtractor={item => item.id.toString()}
-          />
+        <View>
+          <Button onPress={this.handleFetchTeams}>
+            <Text>See all teams</Text>
+          </Button>
+        </View>
+        {this.state.showTeams && (
+          <View>
+            {!teams.length ? (
+              <ActivityIndicator />
+            ) : (
+              <FlatList
+                data={teams}
+                renderItem={({ item, index }) => {
+                  return (
+                    <Team
+                      team={{ ...item, rank: index + 1 }}
+                      index={index}
+                      handleTeamPress={() =>
+                        this.props.navigation.navigate('TeamCard', { item })
+                      }
+                    />
+                  );
+                }}
+                keyExtractor={item => item.id.toString()}
+              />
+            )}
+          </View>
         )}
       </React.Fragment>
     );
